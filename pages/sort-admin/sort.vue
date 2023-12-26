35a36b2154d0b4e73c225fb7abf886c774bd38ad
T1R <2993078435@qq.com>
2023-12-26 16:16:46 +0800

    完成分类管理页面布局，用 page-container 实制作弹窗

---
+++ pages/sort-admin/sort.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="sort-Header sort-position">
+		<text>分类</text>
+		<text>操作</text>
+	</view>
+	<!-- 撑出距离 -->
+	<view style="height: 90rpx;"></view>
+	<view class="sort-Header sort-table">
+		<text class="occupy">膨化食品</text>
+		<text class="sort-but">删除</text>
+	</view>
+	<!-- 弹窗 -->
+	<page-container :show="true" position="bottom" bindenter="onEnter">
+		<view style="height: 900rpx;">111</view>
+	</sort-Header >
+	<!-- 底部新增分类按钮 -->
+	<view style="height: 300rpx;"></view>
+	<view class="newly-added-view">
+		<view class="newly-added">新增分类</view>
+	</view>
+</template>
+
+<script setup>
+	function onEnter(e){
+		console.log(e);
+	}
+</script>
+
+<style scoped>
+.I-style{
+	color: #cccccc;
+}
+.att-input{
+	background-color: #f7f7f7;
+	padding: 20rpx;
+	border-radius: 7rpx;
+}
+.classif{
+	margin: 60rpx 0 !important;
+}
+.modify-sub{
+	padding-bottom: 60rpx !important;
+}
+
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/sort-admin/sort.js
@@ -0,0 +1,12 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  __name: "sort",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return {};
+    };
+  }
+};
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__scopeId", "data-v-a8b65ec7"], ["__file", "E:/Dessert/Dessert-admin/pages/sort-admin/sort.vue"]]);
+wx.createPage(MiniProgramPage);
