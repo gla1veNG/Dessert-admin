f6bf9142e98275e8a191607a84448800e49e6ebc
T1R <2993078435@qq.com>
2023-12-26 11:37:10 +0800

    完成了店名和ä订单收益板块的布局和â局与样式

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,13 +1,122 @@
 <template>
-	<view class="content">
-		首页
+	<view class="Top-view">
+		<view :style=" 'height:' + Custom_height + 'px;' ">
+			<view :style=" 'height:' + S_top + 'px;' "></view>
+			<view :style=" ['height:' + S_height + 'px;','line-height:' + S_height + 'px;', 'padding-left:' + '16px;'] ">Dessert Oasis</view>
+		</view>
+		<!-- 订单收益板块 -->
+		<view class="profit-view" :style=" 'top:' + Pro_height + 'px;'">
+			<view class="profit-num">
+				<text>累计收益(￥)</text>
+				<text>3</text>
+			</view>
+			<view class="profit-sale">
+				<view>
+					<text>今日销售额</text>
+					<text>20</text>
+				</view>
+				<view>
+					<text>今日订单数</text>
+					<text>20</text>
+				</view>
+				<view>
+					<text>累计订单数</text>
+					<text>40</text>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
-<script>
+<script setup>
+	import {onMounted,reactive, toRefs} from 'vue'
+	//存储胶囊按钮位置数据
+	const search_data = reactive({
+		S_height:0,//胶囊按钮高度
+		S_top:0,//胶囊按钮距离顶部高度
+		Custom_height:0,//上两个的和
+		Pro_height:0,//订单收益板块距离手机顶部的高度
+	})
+	
+	const {S_height,S_top,Custom_height,Pro_height} = toRefs(search_data)
+	onMounted(()=>{
+		capSule()
+	})
+	//获取胶囊按钮位置
+	function capSule(){
+		const but_data = wx.getMenuButtonBoundingClientRect();
+		search_data.S_top = but_data.top;
+		search_data.S_height = but_data.height;
+		search_data.Custom_height = but_data.height + but_data.top;
+		search_data.Pro_height = but_data.height + but_data.top + 20;
+	}
 	
 </script>
 
 <style>
-	
+page{background-color: #f4f4f4;}
+.Top-view{
+	height: 330rpx;
+	position: relative;
+	background-image: linear-gradient(to top, #9be15d 0%, #00e3ae 100%);
+}
+.profit-view{
+	background-color: #FFFFFF;
+	position: absolute;
+	left: 20rpx;
+	right: 20rpx;
+	border-radius: 10rpx;
+	padding: 20rpx;
+}
+.profit-num{
+	display: flex;
+	flex-direction: column;
+	padding-bottom: 10rpx;
+}
+.profit-num text:nth-child(2){
+	font-size: 40rpx;
+	padding-top: 10rpx;
+}
+.profit-sale{
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+}
+.profit-sale view{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.profit-sale view text:nth-child(1){
+	color: #a7a7a7;
+	padding-bottom: 10rpx;
+}
+.profit-sale view text:nth-child(2){
+	font-weight: bold;
+}
+.Gong-ge{
+	background-color: #FFFFFF;
+	margin: 0 20rpx 20rpx 20rpx;
+	border-radius: 10rpx;
+	display: flex;
+	flex-wrap: wrap;
+	position: absolute;
+	left: 0;
+	right: 0;
+	color: #555555;
+}
+.Gong-ge image{
+	width: 55rpx;
+	height: 55rpx;
+	display: block;
+	margin-bottom: 20rpx;
+}
+.Gong-ge view{
+	width: calc(25% - 10rpx*2);
+	margin: 20rpx 10rpx;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
 </style>
--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -1,8 +1,40 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return {};
-}
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "E:/Dessert/Dessert-admin/pages/index/index.vue"]]);
+const _sfc_main = {
+  __name: "index",
+  setup(__props) {
+    const search_data = common_vendor.reactive({
+      S_height: 0,
+      //胶囊按钮高度
+      S_top: 0,
+      //胶囊按钮距离顶部高度
+      Custom_height: 0,
+      //上两个的和
+      Pro_height: 0
+      //订单收益板块距离手机顶部的高度
+    });
+    const { S_height, S_top, Custom_height, Pro_height } = common_vendor.toRefs(search_data);
+    common_vendor.onMounted(() => {
+      capSule();
+    });
+    function capSule() {
+      const but_data = common_vendor.wx$1.getMenuButtonBoundingClientRect();
+      search_data.S_top = but_data.top;
+      search_data.S_height = but_data.height;
+      search_data.Custom_height = but_data.height + but_data.top;
+      search_data.Pro_height = but_data.height + but_data.top + 20;
+    }
+    return (_ctx, _cache) => {
+      return {
+        a: common_vendor.s("height:" + common_vendor.unref(S_top) + "px;"),
+        b: common_vendor.s("height:" + common_vendor.unref(S_height) + "px;"),
+        c: common_vendor.s("line-height:" + common_vendor.unref(S_height) + "px;"),
+        d: common_vendor.s("padding-left:16px;"),
+        e: common_vendor.s("height:" + common_vendor.unref(Custom_height) + "px;"),
+        f: common_vendor.s("top:" + common_vendor.unref(Pro_height) + "px;")
+      };
+    };
+  }
+};
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "E:/Dessert/Dessert-admin/pages/index/index.vue"]]);
 wx.createPage(MiniProgramPage);
