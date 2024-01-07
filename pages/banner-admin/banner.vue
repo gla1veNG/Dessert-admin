5c012a6aadf2a6dac9ad9119b143ceb22f10f228
T1R <2993078435@qq.com>
2024-01-07 18:46:08 +0800

    完成 '横幅管理' 页面的图片上传 和 图片的删除

--- pages/banner-admin/banner.vue
+++ pages/banner-admin/banner.vue
@@ -1,13 +1,15 @@
 <template>
-	<view class="sort-Header sort-position">
+	<view class="sort-Header sort-position" v-if="banner_data.length > 0">
 		<text>横幅图片</text>
 		<text>操作</text>
 	</view>
 	<view style="height: 90rpx;"></view>
-	<view class="sort-Header sort-table">
-		<image src="../../static/detail/026.jpg" mode="aspectFill"></image>
+	<view class="sort-Header sort-table" v-for="(item,index) in banner_data" :key="index">
+		<image :src="item.banner_cover" mode="aspectFill"></image>
 		<text class="sort-but">删除</text>
 	</view>
+	<!-- 没有数据的提示 -->
+	<view class="Tips" v-if="banner_data.length === 0">目前没有任何横幅数据</view>
 	<!-- 底部 -->
 	<view style="height:300rpx;"></view>
 	<view class="newly-added-view">
@@ -22,9 +24,9 @@
 				<text>提交</text>
 			</view>
 			<view class="upload-cover">
-				<image src="/static/detail/miaosha-img.jpg" mode="aspectFill"></image>
-				<!-- <image src="/static/detail/026.jpg" mode="aspectFill"></image> -->
-				<image src="/static/detail/shanchu-goods.svg" mode="widthFix"></image>
+				<image src="/static/detail/miaosha-img.jpg" mode="aspectFill" v-if="banner_cover === ''" @click="upImage"></image>
+				<image :src="banner_cover" mode="aspectFill"></image>
+				<image src="/static/detail/shanchu-goods.svg" mode="widthFix" v-if="banner_cover != ''" @click="banner_cover = ''"></image>
 			</view>
 			<view class="relation relation-back">
 				<text>关联商品</text>
@@ -35,12 +37,40 @@
 </template>
 
 <script setup>
-	import {ref} from 'vue'
-	
+	import {ref,onMounted,reactive,toRefs} from 'vue'
+	import {inIt} from '@/Acc-config/init.js'
 	function onEnter(){}
 	
 	const show = ref(false);
 	
+	onMounted(()=>{
+		getBannner();
+	})
+	
+	const data = reactive({
+		banner_data:[],
+		banner_cover:''
+		
+	})
+	
+	const {banner_data,banner_cover} = toRefs(data);
+	
+	//获取数据
+	async function getBannner(){
+		let DB = await inIt();
+		let res = await DB.database().collection('banner').get();
+		data.banner_data = res.data;
+	}
+	//上传轮播图
+	import {Feedback,Upload} from '@/Acc-config/media.js'
+	async function upImage(){
+		const local = await new Upload().image();
+		wx.showLoading({title:'正在上传',mask:true});
+		const res = await new Upload().cloud(local[0].tempFilePath);
+		data.banner_cover = res;
+		wx.hideLoading();
+		
+	} 
 </script>
 
 <style scoped>
--- unpackage/dist/dev/mp-weixin/pages/banner-admin/banner.js
+++ unpackage/dist/dev/mp-weixin/pages/banner-admin/banner.js
@@ -1,15 +1,56 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
+const AccConfig_init = require("../../Acc-config/init.js");
+const AccConfig_media = require("../../Acc-config/media.js");
 const _sfc_main = {
   __name: "banner",
   setup(__props) {
     const show = common_vendor.ref(false);
+    common_vendor.onMounted(() => {
+      getBannner();
+    });
+    const data = common_vendor.reactive({
+      banner_data: [],
+      banner_cover: ""
+    });
+    const { banner_data, banner_cover } = common_vendor.toRefs(data);
+    async function getBannner() {
+      let DB = await AccConfig_init.inIt();
+      let res = await DB.database().collection("banner").get();
+      data.banner_data = res.data;
+    }
+    async function upImage() {
+      const local = await new AccConfig_media.Upload().image();
+      common_vendor.wx$1.showLoading({ title: "正在上传", mask: true });
+      const res = await new AccConfig_media.Upload().cloud(local[0].tempFilePath);
+      data.banner_cover = res;
+      common_vendor.wx$1.hideLoading();
+    }
     return (_ctx, _cache) => {
-      return {
-        a: common_vendor.o(($event) => show.value = true),
-        b: common_vendor.o(($event) => show.value = false),
-        c: show.value
-      };
+      return common_vendor.e({
+        a: common_vendor.unref(banner_data).length > 0
+      }, common_vendor.unref(banner_data).length > 0 ? {} : {}, {
+        b: common_vendor.f(common_vendor.unref(banner_data), (item, index, i0) => {
+          return {
+            a: item.banner_cover,
+            b: index
+          };
+        }),
+        c: common_vendor.unref(banner_data).length === 0
+      }, common_vendor.unref(banner_data).length === 0 ? {} : {}, {
+        d: common_vendor.o(($event) => show.value = true),
+        e: common_vendor.o(($event) => show.value = false),
+        f: common_vendor.unref(banner_cover) === ""
+      }, common_vendor.unref(banner_cover) === "" ? {
+        g: common_vendor.o(upImage)
+      } : {}, {
+        h: common_vendor.unref(banner_cover),
+        i: common_vendor.unref(banner_cover) != ""
+      }, common_vendor.unref(banner_cover) != "" ? {
+        j: common_vendor.o(($event) => banner_cover.value = "")
+      } : {}, {
+        k: show.value
+      });
     };
   }
 };
