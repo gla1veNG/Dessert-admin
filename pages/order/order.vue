961ac97f37a2e84c33c98948355e55d31ed2af99
T1R <2993078435@qq.com>
2024-01-22 14:35:10 +0800

    完成 '订单' 页面 tab切换 、请求到数据在页面展示 和 实现点击发货

--- pages/order/order.vue
+++ pages/order/order.vue
@@ -1,11 +1,276 @@
 <template>
-	<view>
-		订单管理
+	<view class="order-tab">
+		<view v-for="(item,index) in tab" :key="index" @click="swItch(index,item.query)">
+			<text>{{item.name}}</text>
+			<text :class="{order_Select : index == re}"></text>
+		</view>
+	</view>
+	<view style="height: 60rpx;"></view>
+	<!-- 订单 -->
+	<view class="order-back" v-for="(item,index) in order_data" :key="index"> 
+		<view class="order-card">
+			<view>
+				<image :src="item.goods_image" mode="aspectFill"></image>
+			</view>
+			<view>
+				<text class="order-title-padd">{{item.goods_title}}</text>
+				<text class="order-specs" v-if="item.specs.length > 0">{{item.att_val}}</text>
+			</view>
+			<view>
+				<text class="order-title-padd order-num">{{item.goods_price}}</text>
+				<text class="order-num">x{{item.buy_amount}}</text>
+			</view>
+		</view>
+		<!-- 合计 -->
+		<view class="order-total">
+			<text>合计</text>
+			<text>{{item.subtotal}}</text>
+		</view>
+		<!-- 订单状态 -->
+		<!-- 待付款 -->
+		<view class="order-button" v-if="item.pay_success === 'not_pay'">
+			<text class="order-button-a">待付款</text>
+		</view>
+		<!-- 已付款 -->
+		<view class="order-button" v-if="item.pay_success === 'success'">
+			<block v-if="item.deliver === 'stay'">
+				<text class="order-button-b" @click="waybill = true,deliver_id = item._id,deliver_index = index">发货</text>
+			</block>
+			<block v-if="item.deliver === 'already'">
+				<text class="order-button-a">已发货</text>
+			</block>
+			<block v-if="item.deliver === 'ref_pro'">
+				<text class="order-button-b" @click="reFund(index,item.out_trade_no,item.subtotal,item._id)">确认退款</text>
+			</block>
+			<block v-if="item.deliver === 'ref_succ'">
+				<text class="order-button-a">已退款</text>
+			</block>
+		</view>
+	</view>
+	<!-- 没有数据的提示 -->
+	<view class="Tips" v-if="order_data.length === 0">没有订单数据</view>
+	<!-- 上拉加载的提示 -->
+	<!-- <view class="loading-hei">
+		<Loading v-if="loading"></Loading>
+	</view> -->
+	<!-- 填写运单号的弹窗 -->
+	<view class="modify-att" v-if="waybill">
+		<view class="Waybill-view">
+			<input type="text" placeholder="请输入运单号" :focus="true" v-model="deliver_on">
+			<view>
+				<button type="default" size="mini" @click="waybill = false,deliver_on = ''">取消</button>
+				<button type="primary" size="mini" @click="deLiver">确定</button>
+			</view>
+		</view>
 	</view>
 </template>
 
-<script>
+<script setup>
+	import {ref,reactive,toRefs} from 'vue'
+	import {onLoad,onReachBottom} from '@dcloudio/uni-app'
+	import {inIt} from '@/Acc-config/init.js'
+	import Loading from '@/pages/public-view/loading.vue'
+	
+	const data = reactive({
+		tab:[
+			{
+				name:'待付款',
+				query:{pay_success:'not_pay'}
+			},
+			{
+				name:'待发货',
+				query:{pay_success:'success',deliver:'stay'}
+			},
+			{
+				name:'已发货',
+				query:{pay_success:'success',deliver:'already'}
+			},
+			{
+				name:'退款/售后',
+				query:{}
+			}
+		]
+	})
+	const {tab} = toRefs(data)
+	const re = ref(0)
+	// tab切换
+	function swItch(index,query){
+		re.value = index
+		page_n.value = 0
+		res_order.order_data = []
+		getOrder(0,query)
+	}
+	onLoad(async()=>{
+		let DB = await inIt()
+		const _ = DB.database().command
+		data.tab[3].query = {pay_success:'success',deliver:_.or('ref_pro','ref_succ')}
+		getOrder(0,data.tab[0].query)
+	})
+	// 请求数据
+	const res_order = reactive({order_data:[]})
+	const {order_data} = toRefs(res_order)
+	async function getOrder(sk=0,query){
+		let DB = await inIt()
+		const BASE = DB.database()
+		const res = await BASE.collection('order_data').where(query).limit(10).skip(sk).get()
+		res_order.order_data = [...res_order.order_data,...res.data]
+	}
+	// 上拉加载
+	let loading = ref(false)
+	let page_n = ref(0)
+	onReachBottom(async()=>{
+		loading.value = true
+		page_n.value++
+		let sk = page_n.value * 10
+		await getOrder(sk,data.tab[re.value].query)
+		loading.value = false
+	})
+	// 发货
+	const waybill = ref(false)//弹窗显示
+	const deliver_id = ref('')//获取_id
+	const deliver_index = ref(0)//获取的下标
+	const deliver_on = ref('')//input输入框的值
+	async function deLiver(){
+		if(deliver_on.value.split(" ").join("").length === 0)return false
+		waybill.value = false
+		wx.showLoading({title: '发货中',mask:true})
+		let DB = await inIt()
+		const BASE = DB.database()
+		const res = await BASE.collection('order_data').doc(deliver_id.value).update({
+			data:{deliver:'already',waybill_No:deliver_on.value}
+			})
+		res_order.order_data.splice(deliver_index.value,1)
+		wx.hideLoading()
+		deliver_on.value = ''
+	}
 </script>
 
 <style>
+page{
+	background-color: #f4f4f4;
+}
+.order-tab{
+	height: 60rpx;
+	background-color: #f4f4f4;
+	display: flex;
+	justify-content: space-around;
+	color: #000000;
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+}
+.order-tab view{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	position: relative;
+}
+.order_Select{
+	display: block;
+	width: 40rpx;
+	height: 10rpx;
+	border-radius: 50rpx;
+	background-color: #ea4050;
+	position: absolute;
+	bottom: 0;
+}
+/* 订单卡片 */
+.order-back{
+	background-color: #fefefe;
+	padding: 20rpx;
+	margin: 20rpx 0;
+}
+.order-card image{
+	width: 150rpx;
+	height: 150rpx;
+	display: block;
+	border-radius: 8rpx;
+}
+.order-card{
+	display: flex;
+}
+
+.order-card view:nth-child(2){
+	flex: 1;
+	padding: 0 20rpx;
+}
+.order-title-padd{
+	margin-bottom: 20rpx;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	display: -webkit-box;
+	-webkit-line-clamp: 1;
+	-webkit-box-orient: vertical;
+}
+.order-specs{
+	background-color: #fafafa;
+	font-size: 27rpx;
+	color: #767676;
+	padding: 10rpx;
+	border-radius: 9rpx;
+}
+.order-num{
+	color: #8b8c90;
+}
+/* 合计 */
+.order-total{
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+}
+.order-total text{
+	display: block;
+	padding-left: 20rpx;
+}
+.order-total text:nth-child(1){
+	color: #8b8c90;
+}
+.order-total text:nth-child(2){
+	font-weight: bold;
+	font-size: 34rpx;
+}
+/* 按钮 */
+.order-button{
+	display: flex;
+	justify-content: flex-end;
+	margin-top: 30rpx;
+}
+.order-button text{
+	padding: 15rpx 35rpx;
+	margin-left: 20rpx;
+	border-radius: 10rpx;
+}
+.order-button-a{
+	border: 1rpx solid #d7d8d8;
+	color: #8b8c90;
+}
+.order-button-b{
+	background-color: #ea4050;
+	color: #FFFFFF;
+}
+
+/* 运单号 */
+.Waybill-view{
+	position:absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	width: 400rpx;
+	height: 200rpx;
+	background-color: #FFFFFF;
+	padding: 20rpx;
+	border-radius: 10rpx;
+}
+.Waybill-view button{
+	margin: inherit;
+}
+.Waybill-view view{
+	display: flex;
+	justify-content: space-between;
+	position: absolute;
+	bottom: 20rpx;
+	left: 20rpx;
+	right: 20rpx;
+}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/order/order.js
+++ unpackage/dist/dev/mp-weixin/pages/order/order.js
@@ -1,8 +1,129 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return {};
-}
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "E:/Dessert/Dessert-admin/pages/order/order.vue"]]);
+const AccConfig_init = require("../../Acc-config/init.js");
+const _sfc_main = {
+  __name: "order",
+  setup(__props) {
+    const data = common_vendor.reactive({
+      tab: [
+        {
+          name: "待付款",
+          query: { pay_success: "not_pay" }
+        },
+        {
+          name: "待发货",
+          query: { pay_success: "success", deliver: "stay" }
+        },
+        {
+          name: "已发货",
+          query: { pay_success: "success", deliver: "already" }
+        },
+        {
+          name: "退款/售后",
+          query: {}
+        }
+      ]
+    });
+    const { tab } = common_vendor.toRefs(data);
+    const re = common_vendor.ref(0);
+    function swItch(index, query) {
+      re.value = index;
+      page_n.value = 0;
+      res_order.order_data = [];
+      getOrder(0, query);
+    }
+    common_vendor.onLoad(async () => {
+      let DB = await AccConfig_init.inIt();
+      const _ = DB.database().command;
+      data.tab[3].query = { pay_success: "success", deliver: _.or("ref_pro", "ref_succ") };
+      getOrder(0, data.tab[0].query);
+    });
+    const res_order = common_vendor.reactive({ order_data: [] });
+    const { order_data } = common_vendor.toRefs(res_order);
+    async function getOrder(sk = 0, query) {
+      let DB = await AccConfig_init.inIt();
+      const BASE = DB.database();
+      const res = await BASE.collection("order_data").where(query).limit(10).skip(sk).get();
+      res_order.order_data = [...res_order.order_data, ...res.data];
+    }
+    let loading = common_vendor.ref(false);
+    let page_n = common_vendor.ref(0);
+    common_vendor.onReachBottom(async () => {
+      loading.value = true;
+      page_n.value++;
+      let sk = page_n.value * 10;
+      await getOrder(sk, data.tab[re.value].query);
+      loading.value = false;
+    });
+    const waybill = common_vendor.ref(false);
+    const deliver_id = common_vendor.ref("");
+    const deliver_index = common_vendor.ref(0);
+    const deliver_on = common_vendor.ref("");
+    async function deLiver() {
+      if (deliver_on.value.split(" ").join("").length === 0)
+        return false;
+      waybill.value = false;
+      common_vendor.wx$1.showLoading({ title: "发货中", mask: true });
+      let DB = await AccConfig_init.inIt();
+      const BASE = DB.database();
+      await BASE.collection("order_data").doc(deliver_id.value).update({
+        data: { deliver: "already", waybill_No: deliver_on.value }
+      });
+      res_order.order_data.splice(deliver_index.value, 1);
+      common_vendor.wx$1.hideLoading();
+      deliver_on.value = "";
+    }
+    return (_ctx, _cache) => {
+      return common_vendor.e({
+        a: common_vendor.f(common_vendor.unref(tab), (item, index, i0) => {
+          return {
+            a: common_vendor.t(item.name),
+            b: index == re.value ? 1 : "",
+            c: index,
+            d: common_vendor.o(($event) => swItch(index, item.query), index)
+          };
+        }),
+        b: common_vendor.f(common_vendor.unref(order_data), (item, index, i0) => {
+          return common_vendor.e({
+            a: item.goods_image,
+            b: common_vendor.t(item.goods_title),
+            c: item.specs.length > 0
+          }, item.specs.length > 0 ? {
+            d: common_vendor.t(item.att_val)
+          } : {}, {
+            e: common_vendor.t(item.goods_price),
+            f: common_vendor.t(item.buy_amount),
+            g: common_vendor.t(item.subtotal),
+            h: item.pay_success === "not_pay"
+          }, item.pay_success === "not_pay" ? {} : {}, {
+            i: item.pay_success === "success"
+          }, item.pay_success === "success" ? common_vendor.e({
+            j: item.deliver === "stay"
+          }, item.deliver === "stay" ? {
+            k: common_vendor.o(($event) => (waybill.value = true, deliver_id.value = item._id, deliver_index.value = index), index)
+          } : {}, {
+            l: item.deliver === "already"
+          }, item.deliver === "already" ? {} : {}, {
+            m: item.deliver === "ref_pro"
+          }, item.deliver === "ref_pro" ? {
+            n: common_vendor.o(($event) => _ctx.reFund(index, item.out_trade_no, item.subtotal, item._id), index)
+          } : {}, {
+            o: item.deliver === "ref_succ"
+          }, item.deliver === "ref_succ" ? {} : {}) : {}, {
+            p: index
+          });
+        }),
+        c: common_vendor.unref(order_data).length === 0
+      }, common_vendor.unref(order_data).length === 0 ? {} : {}, {
+        d: waybill.value
+      }, waybill.value ? {
+        e: deliver_on.value,
+        f: common_vendor.o(($event) => deliver_on.value = $event.detail.value),
+        g: common_vendor.o(($event) => (waybill.value = false, deliver_on.value = "")),
+        h: common_vendor.o(deLiver)
+      } : {});
+    };
+  }
+};
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "E:/Dessert/Dessert-admin/pages/order/order.vue"]]);
 wx.createPage(MiniProgramPage);
